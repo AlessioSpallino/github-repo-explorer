9418b905d5b1d146469082ff3623864c1523d013
Alessio Spallino <alessio@ellipsishealth.com>
2024-12-10 23:31:30 -0500

    Change type for Repo interface to have only avatar_url

--- types/index.ts
+++ types/index.ts
@@ -1,10 +1,11 @@
-import { RestEndpointMethodTypes } from '@octokit/rest'; // Importing types from Octokit
+type RepoOwner = {
+    avatar_url: string;
+};
 
-type RepoResponseOwnerItem = RestEndpointMethodTypes['search']['repos']['response']['data']['items'][number]['owner'];
 export interface Repo {
     id: number;
     name: string;
-    owner: RepoResponseOwnerItem;
+    owner: RepoOwner;
     html_url: string;
     description: string | null;
     stargazers_count?: number;
